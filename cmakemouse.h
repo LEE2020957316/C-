b758f44071d8bba2b98254e1ce967a33eec17a92
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2023-01-27 23:46:54 +0000

    Create cmakemouse.h

---
+++ cmakemouse.h
@@ -0,0 +1,26 @@
+#include <iostream>
+
+struct Mousecallback {
+public:
+	virtual void hasData(int x, int y, bool left, bool middle, bool right) = 0;
+};
+
+struct MyMouseCallback : Mousecallback {
+public:
+	void hasData(int x, int y, bool left, bool middle, bool right);
+};
+
+class Mouse {
+public:
+	int x;
+	int y;
+	bool left;
+	bool middle;
+	bool right;
+public:
+	Mousecallback* mc;
+public:
+	void registerCallback(Mousecallback* mc);
+	void start();
+	void stop();
+};
