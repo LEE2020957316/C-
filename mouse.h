c6b244ff1c82a7201877066a4150bce93e58256b
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2023-01-30 23:42:41 +0000

    Update mouse.h

--- mouse.h
+++ mouse.h
@@ -5,6 +5,10 @@ public:
 	virtual void hasData(int x, int y, bool left, bool middle, bool right) = 0;
 };
 
+struct MyMouseCallback : Mousecallback {
+public:
+	void hasData(int x, int y, bool left, bool middle, bool right);
+};
 
 class Mouse {
 public:
